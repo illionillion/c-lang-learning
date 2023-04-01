be8efaa782c91cf567e4b4b9352ecfb1591e2b72
yuto ichinose <yuto.0526.sano@gmail.com>
2023-04-01 23:01:13 +0900

    文字コードで８進数書いたりするやつ

---
+++ src/code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    printf(" 8進数の20は10進数で%dです。\n", 020); // これがよー分からん
+    printf("16進数の20は10進数で%dです。\n", 0x20);
+    printf("16進数の Aは10進数で%dです。\n",0x0A);
+}
+
