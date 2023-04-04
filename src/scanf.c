26ee1b210bb03ad8fba01b882a074ce9db22dfe0
yuto ichinose <yuto.0526.sano@gmail.com>
2023-04-04 12:26:59 +0900

    複数の変数に入力させることも可能

--- src/scanf.c
+++ src/scanf.c
@@ -5,7 +5,12 @@ int main (void) {
     char* str ;
     printf("文字列を入力："); 
     scanf("%s", str);
-    printf("入力された文字列%s", str);
+    printf("入力された文字列%s\n", str);
+
+    int n;
+    char c;
+    scanf("%d%c", &n, &c); // まとめて入力も可能
+    printf("n=%d, c=%c\n", n, c);
 
     return 0;
 }
